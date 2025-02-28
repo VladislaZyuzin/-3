89437adeb0424ae29ab58d71d6e8cd17b1a008df
VladislavZyuzin <VLAD_z01@inbox.ru>
2025-02-28 13:06:22 +0300

    Create __init__.py

---
+++ calculator/__init__.py
@@ -0,0 +1,7 @@
+"""
+Пакет calculator даёт функции для выполнения базовых мат. операций.
+"""
+
+from .calculator import add, subtract, multiply, divide
+
+__all__ = ['add', 'subtract', 'multiply', 'divide']
